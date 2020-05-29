805c626c648fa0c0e19d0c2b20da34830b798129
82108 <shinsubin10won@gmail.com>
2020-05-29 13:56:16 +0900

    [ADD] AuthService

---
+++ src/main/java/ssd/pbl/service/AuthService.java
@@ -0,0 +1,37 @@
+package ssd.pbl.service;
+
+import ssd.pbl.model.LoginForm;
+
+public class AuthService {
+	public void login() {
+		
+	}
+	
+	public void logout() {
+		
+	}
+	
+//	public LoginForm findID() {
+//		
+//	}
+	
+//	public LoginForm findPW() {
+//		
+//	}
+	
+	public void resetPW() {
+		
+	}
+	
+//	public boolean isEmailExist() {
+//		
+//	}
+	
+	public void createStudent() {
+		
+	}
+	
+	public void createTeacher() {
+		
+	}
+}
