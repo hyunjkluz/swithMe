b342149893c5d615436d6d4a7c9f9c6a3eb4b008
82108 <shinsubin10won@gmail.com>
2020-05-29 13:58:39 +0900

    [ADD] Form class

---
+++ src/main/java/ssd/pbl/model/FindIDForm.java
@@ -0,0 +1,28 @@
+package ssd.pbl.model;
+
+public class FindIDForm {
+	private String type;
+	private String name;
+	private String phone;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
---
+++ src/main/java/ssd/pbl/model/FindPWForm.java
@@ -0,0 +1,28 @@
+package ssd.pbl.model;
+
+public class FindPWForm {
+	private String type;
+	private String email;
+	private String name;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
