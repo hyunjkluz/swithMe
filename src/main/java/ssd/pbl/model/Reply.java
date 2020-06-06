430b354521255989cf1fdfb8cf5ef58249335a6f
kangeunyoung <youngs8876@gmail.com>
2020-06-06 21:42:06 +0900

    [ADD]reply model

--- src/main/java/ssd/pbl/model/Reply.java
+++ src/main/java/ssd/pbl/model/Reply.java
@@ -1,5 +1,58 @@
 package ssd.pbl.model;
 
-public class Reply {
+import java.io.Serializable;
+import java.util.Date;
 
+@SuppressWarnings("serial")
+public class Reply implements Serializable{
+	int relpyId;
+	String content;
+	int boardId;
+	int writerId;
+	String writerType;
+	Date registerdAt;
+	Date updatedAt;
+	public int getRelpyId() {
+		return relpyId;
+	}
+	public void setRelpyId(int relpyId) {
+		this.relpyId = relpyId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+	public int getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(int writerId) {
+		this.writerId = writerId;
+	}
+	public String getWriterType() {
+		return writerType;
+	}
+	public void setWriterType(String writerType) {
+		this.writerType = writerType;
+	}
+	public Date getRegisterdAt() {
+		return registerdAt;
+	}
+	public void setRegisterdAt(Date registerdAt) {
+		this.registerdAt = registerdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
 }
