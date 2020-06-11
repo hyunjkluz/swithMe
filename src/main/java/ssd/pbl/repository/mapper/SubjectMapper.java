f6facd9b5c589620ad4a49d0442124337d1948f8
김현진 <hyunjk.luz@gmail.com>
2020-06-11 12:21:25 +0900

    [UPDATE] Subject Mapper Query

--- src/main/java/ssd/pbl/repository/mapper/SubjectMapper.java
+++ src/main/java/ssd/pbl/repository/mapper/SubjectMapper.java
@@ -18,6 +18,6 @@ public interface SubjectMapper {
 	Integer insertSubject(@Param("name") String name, @Param("category") String category);
 
 	List<Subject> selectAllSubject();
-
-	List<SubjectTestPaper> selectTestQuestionBySubjectId(int subjectId);
+	
+	List<SubjectTestPaper> selectTestQuestionBySubjectId(@Param("subjectId") int subjectId);
 }
