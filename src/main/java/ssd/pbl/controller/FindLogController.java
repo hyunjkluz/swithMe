1b8ed9c013d2716e75cec56e837dc5ade5826033
subin <subees4136@naver.com>
2020-06-12 14:23:27 +0900

    [UPDATE] loginout, findId, signup controller

--- src/main/java/ssd/pbl/controller/FindLogController.java
+++ src/main/java/ssd/pbl/controller/FindLogController.java
@@ -4,12 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ssd.pbl.model.FindIDForm;
-import ssd.pbl.model.LoginForm;
 import ssd.pbl.service.AuthService;
 
 @Controller
@@ -20,14 +20,20 @@ public class FindLogController {
 	private AuthService authService;
 	
 	@RequestMapping(value = "/id.do", method = RequestMethod.GET)
-	public String getFindIDForm(@Valid @ModelAttribute("findIDForm") FindIDForm findIDForm) {
-//		String id = authService.findID();
-		
-		return "login/FindIDForm";
+	public String getFindIDForm(@Valid @ModelAttribute("findIDForm") FindIDForm findIDForm) {		
+		return "auth/FindIDForm";
 	}
 	
 	@RequestMapping(value = "/pw.do", method = RequestMethod.GET)
 	public String getFindPWForm() {
-		return "login/FindPWForm";
+		return "auth/FindPWForm";
+	}
+	
+	@RequestMapping(value = "/id", method = RequestMethod.POST)
+	public String getFindID(@Valid @ModelAttribute("findIDForm") FindIDForm findIDForm, Model model) {
+		String id = authService.findID(findIDForm);
+		model.addAttribute("id", id);
+		
+		return "auth/FindIDComplete";
 	}
 }
