903a97abea8372382db581462025ee2c45c8b4ae
subin <subees4136@naver.com>
2020-06-12 14:13:39 +0900

    [UPDATE] AuthService

--- src/main/java/ssd/pbl/service/AuthService.java
+++ src/main/java/ssd/pbl/service/AuthService.java
@@ -1,22 +1,71 @@
 package ssd.pbl.service;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ssd.pbl.controller.UserSession;
+import ssd.pbl.exception.IDPWNotMatchingException;
+import ssd.pbl.model.FindIDForm;
 import ssd.pbl.model.LoginForm;
+import ssd.pbl.repository.mapper.StudentMapperRepository;
+import ssd.pbl.repository.mapper.TeacherMapperRepository;
 
 @Service
 public class AuthService {
-	public void login() {
+	
+	@Autowired
+	private StudentMapperRepository studentMapperRepository;
+	@Autowired
+	private TeacherMapperRepository teacherMapperRepository;
+	
+	public AuthService(StudentMapperRepository studentMapperRepository,
+			TeacherMapperRepository teacherMapperRepository) {
+		super();
+		this.studentMapperRepository = studentMapperRepository;
+		this.teacherMapperRepository = teacherMapperRepository;
+	}
+
+	public boolean login(LoginForm loginForm) throws Exception {
+		if (loginForm.getEmail() == null) {
+//			throw new IDPWNotMatchingException();
+			return false;
+		}
+		LoginForm dbLoginForm = studentMapperRepository.selectStudent(loginForm.getEmail());
 		
+		if (dbLoginForm.getPassword().equals(loginForm.getPassword()) && !(loginForm.getPassword() == null)) {
+//			throw new IDPWNotMatchingException();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public UserSession getInfo(LoginForm loginForm) {
+		if (loginForm.getType().equals("student")) {
+			return studentMapperRepository.selectStudentUserInfo(loginForm.getEmail());
+		} else {
+//			return teacherMapperRepository.selectTeacherUserInfo(loginForm.getEmail());
+		}
+		
+		return new UserSession();
 	}
 	
 	public void logout() {
 		
 	}
 	
-//	public LoginForm findID() {
-//		
-//	}
+	public String findID(FindIDForm findIDForm) {
+		if (findIDForm.getType().equals("student")) {
+			System.out.println(studentMapperRepository.selectEmailByNameAndPhone(findIDForm));
+			return studentMapperRepository.selectEmailByNameAndPhone(findIDForm);
+		} else {
+			System.out.println("teacher" + teacherMapperRepository.selectEmailByNameAndPhone(findIDForm));
+			return teacherMapperRepository.selectEmailByNameAndPhone(findIDForm);
+		}
+		
+	}
 	
 //	public LoginForm findPW() {
 //		
@@ -26,9 +75,9 @@ public class AuthService {
 		
 	}
 	
-//	public boolean isEmailExist() {
-//		
-//	}
+	public boolean isEmailExist(String email) {
+		return studentMapperRepository.selectCountStudentEmail(email);
+	}
 	
 	public void createStudent() {
 		
