fbfcc46c2af402319a5663fe20b49f7d9e2a3204
k <k@DESKTOP-PNJ2A83>
2020-06-13 12:48:25 +0900

    [Add] MatchCommand > TeacherMatchForm

--- src/main/java/ssd/pbl/controller/MatchCommand.java
+++ src/main/java/ssd/pbl/controller/MatchCommand.java
@@ -1,31 +1,30 @@
 package ssd.pbl.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+
 import ssd.pbl.model.Gu;
 import ssd.pbl.model.SubjectLevel;
 import ssd.pbl.model.TeacherMatchForm;
 
 public class MatchCommand {
 	private int id;
-	private SubjectLevel[] sublevels;
+	private int subjectId;
+	private String subjectName;
 	private String gender;
-	private String time;
-	private Gu[] gu;
+	private int level;
+	private String dongIds;
+	private ArrayList<Integer> dongIdArr;
+	private List<String> time;
+	private int ch1, ch2, ch3, ch4, ch5;
 	private String intro1;
 	private String intro2;
-	
-	public MatchCommand(TeacherMatchForm tm) {
-		if(tm != null) {
-			id = tm.getId();
-			sublevels = tm.getSubjectLevel();
-			gender = tm.getGender();
-			time = tm.getTime();
-			gu = tm.getGu();
-			intro1 = tm.getIntro1();
-			intro2 = tm.getIntro2();
-		}
+
+	public MatchCommand() {
 	}
-	
-	public MatchCommand() {}
 
 	public int getId() {
 		return id;
@@ -33,14 +32,6 @@ public class MatchCommand {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public SubjectLevel[] getLevel() {
-		return sublevels;
-	}
-
-	public void setLevel(SubjectLevel[] sublevels) {
-		this.sublevels = sublevels;
 	}
 
 	public String getGender() {
@@ -51,20 +42,12 @@ public class MatchCommand {
 		this.gender = gender;
 	}
 
-	public String getTime() {
+	public List<String> getTime() {
 		return time;
 	}
 
-	public void setTime(String time) {
+	public void setTime(List<String> time) {
 		this.time = time;
-	}
-
-	public Gu[] getGu() {
-		return gu;
-	}
-
-	public void setGu(Gu[] gu) {
-		this.gu = gu;
 	}
 
 	public String getIntro1() {
@@ -81,6 +64,93 @@ public class MatchCommand {
 
 	public void setIntro2(String intro2) {
 		this.intro2 = intro2;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public ArrayList<Integer> getDongIdArr() {
+		return dongIdArr;
+	}
+
+	public void setDongIdArr(ArrayList<Integer> dongIdArr) {
+		this.dongIdArr = dongIdArr;
+	}
+
+	public String getDongIds() {
+		return dongIds;
+	}
+
+	public void setDongIds(String dongIds) {
+		this.dongIds = dongIds;
+	}
+
+	public int getCh1() {
+		return ch1;
+	}
+
+	public void setCh1(int ch1) {
+		this.ch1 = ch1;
+	}
+
+	public int getCh2() {
+		return ch2;
+	}
+
+	public void setCh2(int ch2) {
+		this.ch2 = ch2;
+	}
+
+	public int getCh3() {
+		return ch3;
+	}
+
+	public void setCh3(int ch3) {
+		this.ch3 = ch3;
+	}
+
+	public int getCh4() {
+		return ch4;
+	}
+
+	public void setCh4(int ch4) {
+		this.ch4 = ch4;
+	}
+
+	public int getCh5() {
+		return ch5;
+	}
+
+	public void setCh5(int ch5) {
+		this.ch5 = ch5;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentMatchForm [id=" + id + ", subjectId=" + subjectId + ", subjectName=" + subjectName + ", dongIds=" + dongIds + ", dongIdArr="
+				+ dongIdArr + ", level=" + level + ", gender=" + gender + ", time=" + time + ", ch1=" + ch1 + ", ch2=" + ch2 + ", ch3=" + ch3 + ", ch4=" + ch4 + ", ch5=" + ch5
+				+ ", intro1=" + intro1 + ", intro2=" + intro2 + "]";
 	}
 
 }
