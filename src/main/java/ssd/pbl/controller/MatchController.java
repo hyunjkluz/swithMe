8e37a47ed9aa994abef9bd8c0da9e5c1d8b998b6
k <k@DESKTOP-PNJ2A83>
2020-06-13 12:48:07 +0900

    [update] MatchController

--- src/main/java/ssd/pbl/controller/MatchController.java
+++ src/main/java/ssd/pbl/controller/MatchController.java
@@ -1,5 +1,6 @@
 package ssd.pbl.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import ssd.pbl.model.Dong;
+import ssd.pbl.model.Gu;
+import ssd.pbl.model.RadioButton;
+import ssd.pbl.model.Sido;
 import ssd.pbl.model.Subject;
 import ssd.pbl.model.SubjectLevel;
+import ssd.pbl.model.TeacherMatchForm;
+import ssd.pbl.service.CharacterService;
+import ssd.pbl.service.RegionService;
 import ssd.pbl.service.SubjectService;
 
 @Controller
 @RequestMapping("/teacher/match")
-@SessionAttributes("tminfo")
+@SessionAttributes("tmInfo")
 public class MatchController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MatchController.class);
 	
@@ -33,14 +42,75 @@ public class MatchController {
 	private static final String STEP5_FORM_VIEW = "jointeacher/InputMInfoTeacherProfiles";
 	private static final String DONE = "jointeacher/InputMInfoTeacherFinish";
 	
-	@ModelAttribute("tminfo")
-	public MatchCommand formBacking() {
-		MatchCommand tmf = new MatchCommand();
-		return tmf;
-	}   
-	
 	@Autowired
 	private SubjectService subjectService;
+	@Autowired
+	private CharacterService characterService;
+	@Autowired
+	private RegionService regionService;
+	
+	@ModelAttribute("tmInfo")
+	public TeacherMatchForm formBacking() {
+		TeacherMatchForm tmf = new TeacherMatchForm();
+		return tmf;
+	}
+	
+	@ModelAttribute("subjects")
+	public List<Subject> subjFormBacking() {
+		return subjectService.getAllAubject();
+	}
+	
+	@ModelAttribute("sidos")
+	public List<Sido> sidoFormBacking() {
+		return regionService.getAllSido();
+	}
+
+	@ModelAttribute("gus")
+	public List<Gu> guFormBacking() {
+		return regionService.getGuBySidoId(1);
+	}
+
+	@ModelAttribute("dongs")
+	public List<Dong> dongFormBacking() {
+		return regionService.getDongByGuId(1);
+	}
+	
+	@ModelAttribute("levels")
+	public List<RadioButton> levelFormBacking() {
+		return Arrays.asList(new RadioButton("하", 1, null), new RadioButton("중", 2, null),
+				new RadioButton("상", 3, null));
+	}
+
+	@ModelAttribute("genders")
+	public List<RadioButton> genderFormBacking() {
+		return Arrays.asList(new RadioButton("성별 무관", 0, "any"), new RadioButton("여자", 0, "women"),
+				new RadioButton("남자", 0, "man"));
+	}
+	
+	@ModelAttribute("char1")
+	public List<Character> character1FormBacking() {
+		return characterService.getTwoCharacterById(11, 12);
+	}
+
+	@ModelAttribute("char2")
+	public List<Character> character2FormBacking() {
+		return characterService.getTwoCharacterById(13, 14);
+	}
+
+	@ModelAttribute("char3")
+	public List<Character> character3FormBacking() {
+		return characterService.getTwoCharacterById(15, 16);
+	}
+
+	@ModelAttribute("char4")
+	public List<Character> character4FormBacking() {
+		return characterService.getTwoCharacterById(17, 18);
+	}
+
+	@ModelAttribute("char5")
+	public List<Character> character5FormBacking() {
+		return characterService.getTwoCharacterById(19, 20);
+	}
 	
 	@RequestMapping(value = "/form.do", method = RequestMethod.GET)
 	public String step1() {
@@ -55,9 +125,9 @@ public class MatchController {
 	} // step2 > step1
 	
 	@RequestMapping(value = "/step2", method = RequestMethod.POST)
-	public String step2(@Valid @ModelAttribute("tminfo") MatchCommand tmInfo, BindingResult result, 
+	public String step2(@Valid @ModelAttribute("tmInfo") TeacherMatchForm tmInfo, BindingResult result, 
 			HttpServletRequest request) {
-		System.out.println("command 객체: " + tmInfo);
+		System.out.println("command 객체: " + tmInfo.toString());
 		// 유효성 검사
 		return STEP2_FORM_VIEW;
 	}
@@ -68,8 +138,8 @@ public class MatchController {
 	} // step3 > step2
 	
 	@RequestMapping(value = "/step3", method = RequestMethod.POST)
-	public String step3(@Valid @ModelAttribute("tminfo") MatchCommand tmInfo, BindingResult result) {
-		System.out.println("command 객체: " + tmInfo);
+	public String step3(@Valid @ModelAttribute("tmInfo") TeacherMatchForm tmInfo, BindingResult result) {
+		System.out.println("command 객체: " + tmInfo.toString());
 		// 유효성 검사
 		return STEP3_FORM_VIEW;
 	}
@@ -80,8 +150,8 @@ public class MatchController {
 	} // step4 > step3
 	
 	@RequestMapping(value = "/step4", method = RequestMethod.POST)
-	public String step4(@Valid @ModelAttribute("tminfo") MatchCommand tmInfo, BindingResult result) {
-		System.out.println("command 객체: " + tmInfo);
+	public String step4(@Valid @ModelAttribute("tmInfo") TeacherMatchForm tmInfo, BindingResult result) {
+		System.out.println("command 객체: " + tmInfo.toString());
 		return STEP4_FORM_VIEW;
 	}
 	
@@ -91,8 +161,30 @@ public class MatchController {
 	}
 	
 	@RequestMapping(value = "/step5", method = RequestMethod.POST)
-	public String step5(@Valid @ModelAttribute("tmInfo") MatchCommand tmInfo, BindingResult result) {
-		System.out.println("command 객체: " + tmInfo);
+	public String step5(@Valid @ModelAttribute("tmInfo") TeacherMatchForm tmInfo, BindingResult result) {
+		
+		if (tmInfo.getCh1() < 1) {
+			result.rejectValue("ch1", "empty", "둘 중 하나의 유형을 선택해주세요");
+		}
+
+		if (tmInfo.getCh2() < 2) {
+			result.rejectValue("ch2", "empty", "둘 중 하나의 유형을 선택해주세요");
+		}
+
+		if (tmInfo.getCh3() < 3) {
+			result.rejectValue("ch3", "empty", "둘 중 하나의 유형을 선택해주세요");
+		}
+
+		if (tmInfo.getCh4() < 4) {
+			result.rejectValue("ch4", "empty", "둘 중 하나의 유형을 선택해주세요");
+		}
+
+		if (tmInfo.getCh4() < 5) {
+			result.rejectValue("ch5", "empty", "둘 중 하나의 유형을 선택해주세요");
+		}
+
+		
+		System.out.println("command 객체: " + tmInfo.toString());
 		return STEP5_FORM_VIEW;
 	}
 	
@@ -102,7 +194,8 @@ public class MatchController {
 	}
 	
 	@RequestMapping(value = "/fin", method = RequestMethod.POST)
-	public String fin() {
+	public String fin(@Valid @ModelAttribute("tmInfo") TeacherMatchForm tmInfo, BindingResult result,
+			SessionStatus status) {
 		return DONE;
 	}
 		
