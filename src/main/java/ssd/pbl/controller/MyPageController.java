497a840f73dd684b8532f9a245303e50cb392733
k <k@DESKTOP-PNJ2A83>
2020-06-16 02:58:23 +0900

    [update] mypage controller

--- src/main/java/ssd/pbl/controller/MyPageController.java
+++ src/main/java/ssd/pbl/controller/MyPageController.java
@@ -6,14 +6,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("mypage/{id}")
+@RequestMapping("/mypage/")
 public class MyPageController {
 
-	@RequestMapping(value = "/student/main", method = RequestMethod.GET)
-	public String student() {
+	@RequestMapping(value = "/student.do", method = RequestMethod.GET)
+	public String mypagestudent() {
 		return "mypage/MyPageStudent";
 	}
 	
+	@RequestMapping(value = "/student", method = RequestMethod.GET)
+	public String backtomypagestudent() {
+		return "mypage/MyPageStudent";
+	}
+	
+	@RequestMapping(value = "/teacher.do", method = RequestMethod.GET)
+	public String mypageteacher() {
+		return "mypage/MyPageTeacher";
+	}
+	
+	@RequestMapping(value = "/teacher", method = RequestMethod.GET)
+	public String backtomypageteacher() {
+		return "mypage/MyPageTeacher";
+	}
+
+	@RequestMapping(value = "/student/request", method = RequestMethod.GET)
+	public String studentrequest() {
+		return "mypage/MyPageStudentRequest";
+	}
+	
+	@RequestMapping(value = "/teacher/request", method = RequestMethod.GET)
+	public String teacherrequest() {
+		return "mypage/MyPageTeacherRequest";
+	}
+	
+	@RequestMapping(value = "/student/class", method = RequestMethod.GET)
+	public String studentclass() {
+		return "mypage/MyPageStudentClass";
+	}
+	
+	@RequestMapping(value = "/teacher/class", method = RequestMethod.GET)
+	public String teacherclass() {
+		return "mypage/MyPageTeacherClass";
+	}
 	
 	
 	
