623336091334b94fca9c8981c947a365bc630120
김현진 <hyunjk.luz@gmail.com>
2020-06-17 18:17:34 +0900

    [UPDATE] add step in connection card model

--- src/main/java/ssd/pbl/model/ConnectionCard.java
+++ src/main/java/ssd/pbl/model/ConnectionCard.java
@@ -3,35 +3,51 @@ package ssd.pbl.model;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class ConnectionCard implements Serializable{
+public class ConnectionCard implements Serializable {
 	private int connectionId;
+	private String step;
 	private String subject;
 	private String teacher;
 	private String student;
+
 	public int getConnectionId() {
 		return connectionId;
 	}
+
 	public void setConnectionId(int connectionId) {
 		this.connectionId = connectionId;
 	}
+
 	public String getSubject() {
 		return subject;
 	}
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+
 	public String getTeacher() {
 		return teacher;
 	}
+
 	public void setTeacher(String teacher) {
 		this.teacher = teacher;
 	}
+
 	public String getStudent() {
 		return student;
 	}
+
 	public void setStudent(String student) {
 		this.student = student;
 	}
-	
-	
+
+	public String getStep() {
+		return step;
+	}
+
+	public void setStep(String step) {
+		this.step = step;
+	}
+
 }
