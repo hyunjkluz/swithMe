d1641572c84d22c145c997cefabb952d90f12d8c
김현진 <hyunjk.luz@gmail.com>
2020-06-17 23:38:39 +0900

    [ADD] teacher Service

---
+++ src/main/java/ssd/pbl/service/TeacherService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ssd.pbl.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ssd.pbl.model.Teacher;
+import ssd.pbl.repository.mapper.TeacherMapperRepository;
+
+/**
+ * @author kimhyunjin
+ * @date: Jun 17, 2020 11:18:24 PM
+ */
+@Service
+public class TeacherService {
+	@Autowired
+	private TeacherMapperRepository teacherMapperRepo;
+	
+	public Teacher getTeacherByid(Integer teacherId) {
+		return teacherMapperRepo.selectTeacherById(teacherId);
+	}
+
+}
