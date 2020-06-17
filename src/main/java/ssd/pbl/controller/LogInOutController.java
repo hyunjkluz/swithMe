c2a262c922353ea2e145fd0913a2267cf5110f0b
김현진 <hyunjk.luz@gmail.com>
2020-06-17 20:24:42 +0900

    [ADD] mypage login intercepter

--- src/main/java/ssd/pbl/controller/LogInOutController.java
+++ src/main/java/ssd/pbl/controller/LogInOutController.java
@@ -1,9 +1,13 @@
 package ssd.pbl.controller;
 
+
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +28,7 @@ import ssd.pbl.service.AuthService;
 @RequestMapping("/auth")
 @SessionAttributes("userSession")
 public class LogInOutController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogInOutController.class);
 	
 	@Autowired
 	private AuthService authService;
@@ -34,7 +39,8 @@ public class LogInOutController {
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm,
+	public String login(@ModelAttribute("loginForwardAction") String forwardAction,
+			@Valid @ModelAttribute("loginForm") LoginForm loginForm,
 			@RequestParam("type") String type, HttpSession session, BindingResult result, Model model) throws Exception {
 		if (result.hasErrors()) {
 			return "auth/LoginForm";
@@ -45,6 +51,23 @@ public class LogInOutController {
 				UserSession userSession = authService.getInfo(loginForm);
 				userSession.setType(type);
 				session.setAttribute("userSession", userSession);
+				
+				LOGGER.info("이전 URL : " + forwardAction);
+				// 로그인 이전 페이지가 학생 자동매칭이면 다시 원래 페이지로 돌아감.
+				if (forwardAction.contains("/student/match")) {
+					LOGGER.info("자동매칭중");
+					return "redirect:http://localhost:8080/swithMe/student/match/step5";
+				}
+				
+				// 로그인 이전 페이지가 학생/선생님 마이페이지
+				if (forwardAction.contains("/mypage/teacher.do")) {
+					return "redirect:http://localhost:8080/swithMe/mypage/teacher.do";
+				}
+				
+				if (forwardAction.contains("/mypage/student.do")) {
+					return "redirect:http://localhost:8080/swithMe/mypage/student.do";
+				}
+				
 				return "main";
 			} else {
 				return "auth/LoginForm";
