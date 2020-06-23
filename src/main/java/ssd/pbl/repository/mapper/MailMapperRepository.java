46b6aef6d6d114127760cffeaf78bb0c90871e7b
김현진 <hyunjk.luz@gmail.com>
2020-06-23 21:49:01 +0900

    [ADD] mail mapper/repo

---
+++ src/main/java/ssd/pbl/repository/mapper/MailMapperRepository.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ssd.pbl.repository.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author kimhyunjin
+ * @date: Jun 23, 2020 9:47:44 PM
+ */
+@Repository
+public class MailMapperRepository {
+	@Autowired
+	private MailMapper mailMapper;
+
+	Integer deleteAllData() {
+		return mailMapper.deleteAllData();
+	}
+
+}
